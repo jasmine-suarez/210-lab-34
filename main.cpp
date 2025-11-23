ed7885801e765ef2c3ac506d617a70227f3f1c24
Jasmine <jazzyysuarez@gmail.com>
2025-11-23 01:56:34 -0800

    update

--- main.cpp
+++ main.cpp
@@ -108,7 +108,7 @@ int main() {
     // Creates a vector of graph edges/weights
     vector<Edge> edges = {
         // (x, y, w) —> edge from x to y having weight w
-        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,6,2},{5,6,6},{4,5,9},{2,4,4},{2,5,5}
+        {0,1,12},{0,2,8},{0,3,21},{2,3,6},{2,4,4},
     };
 
     // Creates graph
